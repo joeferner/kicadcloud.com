aa6e3a56af757113a43714bbb97b9307fbf3c90a
Joe Ferner <joe@fernsroth.com>
2012-10-23 11:26:13 -0400

    less

--- web/public/scripts/browserifyEntry.js
+++ web/public/scripts/browserifyEntry.js
@@ -1,4 +1,5 @@
 
 exports.kicad2svg = require('kicad2svg');
+exports.sf = require('sf');
 
 document.require = require;
--- web/public/scripts/schematicDepot.js
+++ web/public/scripts/schematicDepot.js
@@ -2,14 +2,14 @@
 
 var kicad2svg = document.require('kicad2svg');
 
-function renderPcbModule(targetId, pcbModuleText, options) {
+function renderPcbModule(targetSelector, pcbModuleText, options) {
   var mod = kicad2svg.modParser.parseModule(pcbModuleText);
   var svg = kicad2svg.modToSvg(mod, options);
-  $('#' + targetId).html(svg);
+  $(targetSelector).html(svg);
 }
 
-function renderSchematicSymbol(targetId, pcbModuleText, options) {
+function renderSchematicSymbol(targetSelector, pcbModuleText, options) {
   var lib = kicad2svg.libParser.parseSymbolDef(pcbModuleText);
   var svg = kicad2svg.libToSvg(lib, options);
-  $('#' + targetId).html(svg);
+  $(targetSelector).html(svg);
 }
