ffd9aea0fef8cbd5b37849fd865576f6422f5f76
Joe Ferner <joe@fernsroth.com>
2012-10-23 17:16:57 -0400

    up down vote

--- web/public/scripts/schematicDepot.js
+++ web/public/scripts/schematicDepot.js
@@ -3,12 +3,14 @@
 var kicad2svg = document.require('kicad2svg');
 
 function renderPcbModule(targetSelector, pcbModuleText, options) {
+  pcbModuleText = (pcbModuleText || '').trim();
   var mod = kicad2svg.modParser.parseModule(pcbModuleText);
   var svg = kicad2svg.modToSvg(mod, options);
   $(targetSelector).html(svg);
 }
 
 function renderSchematicSymbol(targetSelector, pcbModuleText, options) {
+  pcbModuleText = (pcbModuleText || '').trim();
   var lib = kicad2svg.libParser.parseSymbolDef(pcbModuleText);
   var svg = kicad2svg.libToSvg(lib, options);
   $(targetSelector).html(svg);
