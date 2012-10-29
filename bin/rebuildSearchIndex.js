abe2d1a25edbb5e1a482618759245e70d97a06b7
Joe Ferner <joe@fernsroth.com>
2012-10-29 11:53:21 -0400

    more search

--- bin/rebuildSearchIndex.js
+++ bin/rebuildSearchIndex.js
@@ -2,8 +2,11 @@
 
 'use strict';
 
-var optimist = require('optimist');
 var search = require('../lib/search');
+var async = require('async');
+var persist = require('persist');
+var optimist = require('optimist');
+var models = require('../lib/models');
 
 var args = optimist
   .alias('h', 'help')
@@ -15,14 +18,44 @@ if (args.help) {
   return process.exit(-1);
 }
 
-search.index(1, 'this is a test', function(err) {
+return search.query('C', {}, function(err, items) {
   if (err) {
-    return console.error('index', err.stack);
+    return console.error('query', err.stack);
   }
-  return search.query('test', {}, function(err, items) {
-    if (err) {
-      return console.error('query', err.stack);
-    }
-    return console.log(items);
-  })
+  return console.log(items);
 });
+
+/*
+run(function(err) {
+  if (err) {
+    console.error("Could not rebuild search index", err.stack);
+    return process.exit(-1);
+  }
+  return process.exit(0);
+});
+
+function run(callback) {
+  persist.connect(function(err, conn) {
+    if (err) {
+      return callback(err);
+    }
+
+    models.EdaItem.all(conn, function(err, items) {
+      if (err) {
+        return callback(err);
+      }
+
+      return async.forEachSeries(items, function(item, callback) {
+        var text = item.title + ' ' + item.description + ' ' + item.keywords;
+        return search.index(item.id, text, callback);
+      }, function(err) {
+        if (err) {
+          return callback(err);
+        }
+        conn.close();
+        return callback();
+      });
+    });
+  });
+}
+*/
