7c588c01f615027de95329cd6c5159a0cb096f65
Joe Ferner <joe@fernsroth.com>
2013-01-08 09:14:42 -0500

    increase description size

---
+++ migrations/20130108141109-increaseDescriptionSize.js
@@ -0,0 +1,10 @@
+var dbm = require('db-migrate');
+var type = dbm.dataType;
+
+exports.up = function(db, callback) {
+  return db.changeColumn('edaItems', 'description', {type: type.STRING, length: 60000 }, callback);
+};
+
+exports.down = function(db, callback) {
+
+};
